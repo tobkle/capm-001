f7a78a8b23bf267a97f22274bc9381f677bb5525
Tobias Klemmer <tobias@klemmer.info>
2024-07-01 13:28:12 +0200

    initial

---
+++ .devcontainer/scripts/install-dependencies.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Install SAP dependencies
+
+npm install -g @sap/cds-dk typescript ts-node
+npm install -g mbt@1.2.24
+
+# Install package.json dependencies
+npm install
+
+# Install Clound Foundry CLI
+wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
+echo "deb https://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
+sudo apt-get update
+sudo apt-get install cf8-cli
+cf install-plugin -f https://github.com/cloudfoundry-incubator/multiapps-cli-plugin/releases/latest/download/multiapps-plugin.linux64
+
+# Start server
+npm start
---
+++ README.md
@@ -0,0 +1,7 @@
+# SAP Cloud Application Programming - Development Container Template
+
+A template for a development container for SAP Cloud Application Programming Model
+
+```zsh
+git clone https://github.com/tobkle/capm-devcontainer-template.git
+```
